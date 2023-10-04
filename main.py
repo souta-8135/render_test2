f644af1d879e09664d88d5091a586b2b6089514f
souta-8135 <56578340+souta-8135@users.noreply.github.com>
2023-10-04 11:14:34 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-def index():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
